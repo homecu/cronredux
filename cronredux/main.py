0e54f7a232233e2e2c9c63bd9fd764eb188ebdee
Kenny Ballou <kballou@devnulllabs.io>
2018-02-01 11:09:53 -0700

    main: pep-8 style fixes

--- cronredux/main.py
+++ cronredux/main.py
@@ -40,8 +40,9 @@ class CronReduxCommand(shellish.Command):
         self.add_argument('--XXX-backlog-warning', type=float, default=60,
                           help='Time in seconds before a warning is generated '
                           'about a delayed task execution due to a backlog.')
-        self.add_argument('--max-concurrency', type=int, default=10, help='Max tasks '
-                          'that will be allowed to run concurrently.')
+        self.add_argument('--max-concurrency', type=int, default=10,
+                          help='Max tasks that will be allowed to'
+                          'run concurrently.')
         self.add_argument('--allow-overlap', action='store_true')
         self.add_argument('--plain', action='store_true')
 
@@ -57,13 +58,15 @@ class CronReduxCommand(shellish.Command):
             for spec, command in cronparser.parsef(f):
                 if args.verbose:
                     shellish.vtmlprint("<b>Adding task:</b> <blue>%s</blue> %s"
-                                       % (spec, command), plain=cronredux.PLAIN_OUTPUT)
+                                       % (spec, command),
+                                       plain=cronredux.PLAIN_OUTPUT)
                 tasks.append(scheduler.Task(crontab.CronTab(spec), command))
         if args.slack_webhook:
-            notifier = notification.SlackNotifier(args.slack_webhook,
-                                                  channel=args.slack_channel,
-                                                  username=args.slack_username,
-                                                  icon_emoji=args.slack_icon_emoji)
+            notifier = notification.SlackNotifier(
+                args.slack_webhook,
+                channel=args.slack_channel,
+                username=args.slack_username,
+                icon_emoji=args.slack_icon_emoji)
         else:
             notifier = notification.PrintNotifier()
         loop = asyncio.get_event_loop()
